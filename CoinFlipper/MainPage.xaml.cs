4e86638cf65a0da53fc8e9979176a69faa21d977
marinho777 <danielmarinhom@gmail.com>
2024-04-25 12:38:05 -0300

    chore: first commit

---
+++ CoinFlipper/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace CoinFlipper
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnFlipClicked(object sender, EventArgs e)
+        {
+            int selectedIndex = Convert.ToInt32(PickerCoin.SelectedIndex);
+            string selected = PickerCoin.Items[selectedIndex];
+            Random rand = new Random();
+            string[] options = ["Heads", "Tails"];
+            int optionsIndex = rand.Next(options.Length);
+            string last = options[optionsIndex];
+
+            string source = last.ToLower() + ".png";
+            ImgCoin.Source = source;
+
+            if (selected == last)
+            {
+                WrongRight.Text = "You won";
+            }
+            else
+            {
+                WrongRight.Text = "You lost";
+            }
+        }
+    }
+
+}
